4aca6f8f68b212d0904933eab53bdb6c0a99484a
far11ven <kbhalaik@qualys.com>
2019-09-11 17:01:33 +0530

    added /model/user.js and bcrypt

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require("body-parser");
 const express_graphql = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const Event = require("./models/event");
+const User = require("./models/user");
 
 const app = express();
 const port = process.env.port || 3000;
@@ -21,17 +23,27 @@ app.use(
             price: Float!
             date: String!
         }
+        type User{
+          _id: ID!
+          email: String!
+          password: String
+         }
         input EventInput{
             title: String!
             description: String!
             price: Float!
             date: String!
         }
+        input UserInput{
+          email: String!
+          password: String!
+        }
         type RootQuery{
             events: [Event!]!
         }
         type RootMutation{
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput) : User
         }
         schema {
             query: RootQuery
@@ -76,6 +88,24 @@ app.use(
           })
           .catch(err => {
             console.log(err);
+            throw err;
+          });
+      },
+      createUser: args => {
+        return bcrypt
+          .hash(args.userInput.password, 12)
+          .then(hashedPassword => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword
+            });
+            return user.save();
+          })
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: result.id };
+          })
+          .catch(err => {
             throw err;
           });
       }
