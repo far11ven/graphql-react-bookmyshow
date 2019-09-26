be6b0a85fb4906c8f9207c6e245bd023fda5b162
far11ven <kushalbhalaik@gmail.com>
2019-09-26 15:58:11 +0530

    changed port to PORT
    changed port to PORT

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const graphqlSchema = require("./graphql/schema/index");
 const graphqlResolver = require("./graphql/resolvers/index");
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
